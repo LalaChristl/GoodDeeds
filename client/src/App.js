2ecdff8f687bd45d19f8c93e0ab634baa1bf4a71
LalaChristl <lesleyannchristl@gmail.com>
2023-02-21 11:23:42 +0100

    changed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,7 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HelperRegister from "./components/HelperRegister";
-import HelperLogin from "./components/HelperLogin";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/helperlogin" element={<HelperLogin />} />
-        <Route path="/helperregister" element={<HelperRegister />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div></div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ContextProvider from "./components/Context";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HelperRegister from "./components/HelperRegister";
+import HelperLogin from "./components/HelperLogin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ContextProvider>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/helperlogin" element={<HelperLogin />} />
+        <Route path="/helperregister" element={<HelperRegister />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </ContextProvider>
 );
