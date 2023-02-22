d64cd25e12d43074850e51a6c6b4d59823d12a37
Ajanitone <ajanitone@freenet.de>
2023-02-22 09:36:26 +0100

    added map function

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import ContextProvider from "./components/Context";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HelperRegister from "./components/HelperRegister";
 import HelperLogin from "./components/HelperLogin";
+import Map from "./components/Map";
+import MarkerList from "./components/MarkerList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +17,8 @@ root.render(
         <Route path="/helperlogin" element={<HelperLogin />} />
         <Route path="/helperregister" element={<HelperRegister />} />
         <Route path="/" element={<App />} />
+        <Route element={<Map />} path="/map" />
+        <Route element={<MarkerList />} path="/markerlist"></Route>
       </Routes>
     </BrowserRouter>
   </ContextProvider>
