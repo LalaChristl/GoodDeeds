fbbcb51ff05f4a82137ec55930eb1f456ccbe484
LalaChristl <lesleyannchristl@gmail.com>
2023-03-15 09:27:18 +0100

    finished about page

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -5,6 +5,7 @@ import Boi from "../images/boi.jpg";
 import Lala from "../images/lala.jpg";
 import Maya from "../images/angelou.jpg";
 import GD from "../images/good-deeds.png";
+import Card from "react-bootstrap/Card";
 
 const AboutUs = () => {
   return (
@@ -47,22 +48,16 @@ const AboutUs = () => {
               required a long-term commitment, which didn't fit with our busy
               schedules. We realized that there was a gap in the market for a
               flexible, short-term volunteer organization that could match
-              volunteers with a variety of different people and projects.
-              Inspired by this idea, we decided to start our own volunteer
-              organization. We reached out to friends and acquaintances who were
-              also interested in volunteering but faced similar challenges.
-              Together, we formed a small team and started researching the needs
-              of our community. We found that there are so many people that
-              could benefit from short-term volunteer help. Next, we set out to
-              recruit volunteers. We created a website to spread the word and
-              promote our cause. We are now able to match volunteers with a
-              variety of people and projects in their community. Over time, we
-              hope our organization will grow and we are able to expand our
-              services to other communities in need. We aim to have a reputation
-              for providing high-quality volunteer experiences and for making a
-              meaningful impact on our community. Today, our volunteer
-              organization is starting to thrive, thanks to the hard work and
-              dedication of our team and the volunteers who support us.
+              volunteers with a variety of different people and projects. So, we
+              decided to start our own volunteer organization. Together, we
+              formed a small team and started researching the needs of our
+              community. We found that there are so many people that could
+              benefit from short-term volunteer help. We created a website to
+              spread the word and promote our cause. We are now able to match
+              volunteers with a variety of people in their community. Over time,
+              we hope our organization will grow and we are able to expand our
+              services to other communities in need and make a meaningful impact
+              on the world.
             </p>
           </div>
           <div className="about-us-history-div-2">
@@ -73,25 +68,58 @@ const AboutUs = () => {
       <section>
         <div className="about-us-team-container">
           <div>
-            <h3 className="about-us-team-h3">Founding Members</h3>
+            <h3 className="about-us-team-h3">Leadership Members</h3>
           </div>
-          <div className="about-us-team-images">
-            <div>
-              <img src={Lala} alt="" className="team-image" />
-              <p>Lesley-Ann Christl</p>
-            </div>
-            <div>
-              <img src={Kirk} alt="" className="team-image" />
-              <p>Kirk McDowell</p>
-            </div>
-            <div>
-              <img src={Boi} alt="" className="team-image" />
-              <p>Boi Schrader</p>
-            </div>
-            <div>
-              <img src={Rami} alt="" className="team-image" />
-              <p>Rami Basheer</p>
-            </div>
+
+          <div className="about-us-team-cards">
+            <Card className="about-us-card">
+              <Card.Img
+                className="about-us-team-images"
+                variant="top"
+                src={Lala}
+              />
+              <Card.Body className="flex justify-center items-center">
+                <Card.Text className="about-us-card-text">
+                  Lesley-Ann Christl
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card className="about-us-card">
+              <Card.Img
+                className="about-us-team-images"
+                variant="top"
+                src={Kirk}
+              />
+              <Card.Body className="flex justify-center items-center">
+                <Card.Text className="about-us-card-text">
+                  Kirk McDowell
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card className="about-us-card">
+              <Card.Img
+                className="about-us-team-images"
+                variant="top"
+                src={Rami}
+              />
+              <Card.Body className="flex justify-center items-center">
+                <Card.Text className="about-us-card-text">
+                  Rami Basheer
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card className="about-us-card">
+              <Card.Img
+                className="about-us-team-images"
+                variant="top"
+                src={Boi}
+              />
+              <Card.Body className="flex justify-center items-center">
+                <Card.Text className="about-us-card-text">
+                  Boi Schrader
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </section>
