6a81367ddd139c455c7e1664a12b3f0f074d5d3e
Ajanitone <ajanitone@freenet.de>
2023-03-24 11:52:46 +0100

    edit addTask

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link, useNavigate, useLocation } from "react-router-dom";
 import { Context } from "./Context";
 import Navbar from "./Navbar";
-import "./Dashboard.css"
+import "./Dashboard.css";
 import TaskConfirm from "./TaskConfirm";
 
 const Dashboard = () => {
@@ -11,13 +11,16 @@ const Dashboard = () => {
   const { state, dispatch } = useContext(Context);
   const [user, setUser] = useState(null);
   const [tasks, setTasks] = useState([]);
-
-  console.log(tasks);
-  console.log(state.user._id);
+  const location = useLocation();
+  console.log("dashboard task", tasks);
+  console.log("dashboard user.id", state.user._id);
 
   const { id } = useParams();
 
   useEffect(() => {
+    console.log("Dashboard mounted with id:", id);
+    console.log("Location:", location.pathname, location.search);
+
     const fetchData = async () => {
       try {
         const response = await axios.get("/users/getuser2/" + id);
@@ -32,7 +35,7 @@ const Dashboard = () => {
     };
 
     fetchData();
-  }, [id]);
+  }, [id, location]);
 
   const handleLogout = async () => {
     const response = await axios.get("/users/logout");
@@ -46,66 +49,67 @@ const Dashboard = () => {
   };
 
   return (
-    <>
-    <Navbar />
-<div class="dashboard-container">
-  <div class="dashboard-card">
-    {user && (
-      <>
-        <h2 class="dashboard-heading">
-          Welcome to your Dashboard, {user.firstName}!
-        </h2>
-        <img src={user.image} alt="" class="dashboard-image" />
+    <div key={id}>
+      <Navbar />
+      <div class="dashboard-container">
+        <div class="dashboard-card">
+          {user && (
+            <>
+              <h2 class="dashboard-heading">
+                Welcome to your Dashboard, {user.firstName}!
+              </h2>
+              <img src={user.image} alt="" class="dashboard-image" />
 
-        <div class="dashboard-tasks">
-          <div class="dashboard-tasks-header">
-            <h1 class="dashboard-tasks-heading">My Tasks</h1>
-          </div>
-          {tasks.length > 0 ? (
-            <ul class="dashboard-tasks-list">
-              {tasks.map(
-                (task) =>
-                  task.owner._id === state.user._id && (
-                    <li key={task._id} class="dashboard-task">
-                      <h3 class="dashboard-task-heading">{task.task}</h3>
-                      <p class="dashboard-task-details">{task.taskDetails}</p>
-                      <p class="dashboard-task-location">
-                        Location: {task.location}
-                      </p>
-                      <p class="dashboard-task-date">
-                        Date: {task.taskDate} | Time: {task.taskTime}
-                      </p>
-                    </li>
-                  )
-              )}
-            </ul>
-          ) : (
-            <p class="dashboard-no-tasks">No tasks found.</p>
+              <div class="dashboard-tasks">
+                <div class="dashboard-tasks-header">
+                  <h1 class="dashboard-tasks-heading">My Tasks</h1>
+                </div>
+                {tasks.length > 0 ? (
+                  <ul class="dashboard-tasks-list">
+                    {tasks.map(
+                      (task) =>
+                        task.owner._id === state.user._id && (
+                          <li key={task._id} class="dashboard-task">
+                            <h3 class="dashboard-task-heading">{task.task}</h3>
+                            <p class="dashboard-task-details">
+                              {task.taskDetails}
+                            </p>
+                            <p class="dashboard-task-location">
+                              Location: {task.location}
+                            </p>
+                            <p class="dashboard-task-date">
+                              Date: {task.taskDate} | Time: {task.taskTime}
+                            </p>
+                          </li>
+                        )
+                    )}
+                  </ul>
+                ) : (
+                  <p class="dashboard-no-tasks">No tasks found.</p>
+                )}
+              </div>
+            </>
           )}
+
+          <Link to="/addtasks" class="dashboard-button">
+            Request Help
+          </Link>
+
+          <Link to="/listtasks" class="dashboard-button">
+            Tasks List
+          </Link>
+
+          <Link to={`/helpeeprofile/getuser2/${id}`} class="dashboard-button">
+            Go to profile
+          </Link>
+
+          <button onClick={handleLogout} class="dashboard-logout-button">
+            Logout
+          </button>
         </div>
-      </>
-    )}
-
-    <Link to="/addtasks" class="dashboard-button">
-      Request Help
-    </Link>
-
-    <Link to="/listtasks" class="dashboard-button">
-      Tasks List
-    </Link>
-
-    <Link to={`/helpeeprofile/getuser2/${id}`} class="dashboard-button">
-      Go to profile
-    </Link>
-
-    <button onClick={handleLogout} class="dashboard-logout-button">
-      Logout
-    </button>
-  </div>
-  <TaskConfirm />
-</div>
-
-    </>
+        <TaskConfirm />
+      </div>
+    </div>
   );
 };
 
