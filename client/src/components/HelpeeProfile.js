353b7e8642d06628a7a2e09d142a029e42e476db
Rami B <ramibasheer9@gmail.com>
2023-03-27 21:25:50 +0200

    fixed profile and about us

--- client/src/components/HelpeeProfile.js
+++ client/src/components/HelpeeProfile.js
@@ -5,7 +5,9 @@ import { Context } from "./Context";
 import Navbar from "./Navbar";
 import Footer2 from "./Footer2";
 import "./Profile.css";
-
+import {
+  Box
+} from "@mui/material";
 const HelpeeProfile = () => {
   const navigate = useNavigate();
   const { state, dispatch } = useContext(Context);
@@ -47,20 +49,53 @@ const HelpeeProfile = () => {
   return (
     <>
       <Navbar />
+      <Box
+        sx={{
+          height: "vh",
+          display: "flex",
+          maxWidth: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingBottom: 30,
+          background: "linear-gradient(90deg, rgba(0,82,70,1) 0%, rgba(196,252,240,1) 100%)",
+          color: "black",
+        }}
+    >
       <div className="flex justify-center">
         <div className="helpee-profile-container">
           {user && (
             <>
               <img src={user.image} alt="" className="profile-image" />
               <p className="helpee-profile-name">{user.firstName}</p>
-              <div className="profile-text-container">
-                <p className="profile-text">Name: {user.firstName}</p>
-                <p className="profile-text">Last Name: {user.lastName}</p>
-                <p className="profile-text">Age: {user.age}</p>
-                <p className="profile-text">Gender: {user.gender}</p>
-                <p className="profile-text">Language(s): {user.languages}</p>
-                <p className="profile-text">About Me: {user.about}</p>
-              </div>
+              <div class="profile-text-container">
+  <table>
+    <tr>
+      <td>Name:</td>
+      <td>{user.firstName}</td>
+    </tr>
+    <tr>
+      <td>Last Name:</td>
+      <td>{user.lastName}</td>
+    </tr>
+    <tr>
+      <td>Age:</td>
+      <td>{user.age}</td>
+    </tr>
+    <tr>
+      <td>Gender:</td>
+      <td>{user.gender}</td>
+    </tr>
+    <tr>
+      <td>Language(s):</td>
+      <td>{user.languages}</td>
+    </tr>
+    <tr>
+      <td>About Me:</td>
+      <td>{user.about}</td>
+    </tr>
+  </table>
+</div>
 
               <button onClick={handleEdit} className="profile-button">
                 Edit Profile
@@ -75,6 +110,7 @@ const HelpeeProfile = () => {
           </Link>
         </div>
       </div>
+    </Box>
       <Footer2 />
     </>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,19 +7,16 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Logo3 from "../images/logo-3.png";
 import { Context } from "./Context";
-import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-
-
-
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   appBar: {
-    backgroundColor: "#ff7e36",
+    backgroundColor: "#FF7E36",
     position: "fixed",
     top: 0,
     left: 0,
@@ -28,81 +25,66 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     marginRight: theme.spacing(2),
     height: "50px",
-    
   },
   linkContainer: {
     flexGrow: 1,
     display: "flex",
     justifyContent: "flex-end",
-    [theme.breakpoints.down('sm')]: {
-      display: "none"
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
-   
   },
   link: {
     color: "white",
     textTransform: "uppercase",
     marginRight: theme.spacing(2),
     fontSize: "14px",
-    fontWeight: 'bold',
+    fontWeight: "bold",
     "&:hover": {
       color: "black",
-      backgroundColor: "#ffa472",
+      backgroundColor: "#FFA472",
       // fontSize: "16px",
     },
   },
   menuButton: {
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up("md")]: {
       display: "none",
     },
   },
-  
 }));
-
 function Navbar() {
   const classes = useStyles();
   const navigate = useNavigate();
   const { state, dispatch } = useContext(Context);
-
   const [data, setData] = useState({
     email: "",
     password: "",
   });
-
   const [anchorEl, setAnchorEl] = useState(null);
-
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
-
   const handleLogin = async () => {
     const response = await axios.post("/users/login", data);
-
     if (response.data.success) {
       dispatch({
         type: "login",
         payload: response.data.user,
       });
-
       navigate(`/dashboard/helpeeProfile/getuser2/${response.data.user._id}`);
     }
   };
-
   const handleLogout = async () => {
     const response = await axios.get("http://localhost:5000/users/logout");
-    console.log("🦩 ~ handleLogout ~ response", response);
-
+    console.log(":flamingo: ~ handleLogout ~ response", response);
     dispatch({
       type: "logout",
     });
-
     navigate("/");
   };
-
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
@@ -113,98 +95,105 @@ function Navbar() {
               <img className="" src={Logo3} alt="" />
             </div>
           </Link>
-
           <div className={classes.linkContainer}>
             <Button component={Link} to="/" className={classes.link}>
               Home
             </Button>
-
             <Button component={Link} to="/aboutus" className={classes.link}>
               About
             </Button>
-
             <Button component={Link} to="/contact" className={classes.link}>
               Contact
             </Button>
-
-            <Button component={Link} to="/dashboard/helpeeprofile/getuser1" className={classes.link}>
+            <Button
+              component={Link}
+              to={"/dashboard/helpeeprofile/getuser2/" + state.user._id}
+              className={classes.link}
+            >
               Dashboard
             </Button>
-
-                {state.isAuthenticated ? (
-                <Button className={classes.link} onClick={handleLogout}>
+            {state.isAuthenticated ? (
+              <Button className={classes.link} onClick={handleLogout}>
                 Logout
-                </Button> ) 
-                : 
-                (<>
+              </Button>
+            ) : (
+              <>
                 <Button
-                              aria-controls="simple-menu"
-                              aria-haspopup="true"
-                              onClick={handleMenu}
-                              className={classes.menuButton}
-                            >
-                <MenuIcon />
+                  aria-controls="simple-menu"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  className={classes.menuButton}
+                >
+                  <MenuIcon />
                 </Button>
-
-              <Menu
-                             id="simple-menu"
-                             anchorEl={anchorEl}
-                             keepMounted
-                             open={Boolean(anchorEl)}
-                             onClose={handleClose}
-                           >
-                <MenuItem onClick={handleClose}>
-                  <Link to="/" className={classes.link}>
-                    Home
-                  </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <Link to="/aboutus" className={classes.link} >
-                    About
-                  </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <Link to="/contact" className={classes.link}>
-                    Contact
-                  </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                  <Link to="/map" className={classes.link}>
-                    Contact
-                  </Link>
-                </MenuItem>
-
-                <MenuItem onClick={handleClose}>
-                  <Link 
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/" className={classes.link}>
+                      Home
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/aboutus" className={classes.link}>
+                      About
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/contact" className={classes.link}>
+                      Contact
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/map" className={classes.link}>
+                      Contact
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link
                       to={"/dashboard/helpeeprofile/getuser2/" + state.user._id}
-                      className={classes.link}>
-                    Dashboard
-                  </Link>
-                </MenuItem>
-              </Menu>
-
-              {state.user._id ? (
-              <Button
-                  onClick={handleLogout} component={Link} to="/" className={classes.link}>
+                      className={classes.link}
+                    >
+                      Dashboard
+                    </Link>
+                  </MenuItem>
+                </Menu>
+                {state.user._id ? (
+                  <Button
+                    onClick={handleLogout}
+                    component={Link}
+                    to="/"
+                    className={classes.link}
+                  >
                     Logout
-              </Button>
-              ) : (
-              <Button
-                  onClick={handleLogin} component={Link} to="/login" className={classes.link}>
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={handleLogin}
+                    component={Link}
+                    to="/login"
+                    className={classes.link}
+                  >
                     Login
-              </Button>
-              )}
-
-              <Button component={Link} to="/register" className={classes.link}>
-                Register
-              </Button>
-            </>
+                  </Button>
+                )}
+                <Button
+                  component={Link}
+                  to="/register"
+                  className={classes.link}
+                >
+                  Register
+                </Button>
+              </>
             )}
           </div>
         </Toolbar>
       </AppBar>
     </div>
- );
+  );
 }
-              
 export default Navbar;
