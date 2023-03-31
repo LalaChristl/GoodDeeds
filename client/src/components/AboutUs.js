1bd0593ea2e50449a73c09b4c19a8ad683324c54
Ajanitone <ajanitone@freenet.de>
2023-03-31 11:45:04 +0200

    mediaquery about-page

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -4,15 +4,12 @@ import Rami from "../images/rami.jpg";
 import Boi from "../images/boi.jpg";
 import Lala from "../images/lala.jpg";
 import Maya from "../images/angelou.jpg";
-import GD from "../images/good-deeds.png";
+import GD from "../images/about-us-logo.png";
 import Card from "react-bootstrap/Card";
 import Navbar from "./Navbar";
 import Footer2 from "./Footer2";
 
-
-import {
-  Box
-} from "@mui/material";
+import { Box } from "@mui/material";
 
 const AboutUs = () => {
   return (
@@ -20,131 +17,134 @@ const AboutUs = () => {
       <div className="border-2  gap-5 max-w-[full] mx-auto min-w-[360px] overflow-hidden items-center bg-[#fff3e9] text-[#110931]">
         <Navbar />
         <Box
-        sx={{
-          height: "vh",
-          display: "flex",
-          maxWidth: "100%",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          paddingBottom: 30,
-          background:  "linear-gradient(90deg, rgba(255,232,210,1) 0%, rgba(196,252,240,1) 100%)",
-          color: "black",
-        }}
-    >
-        <section>
-          <div className="about-us-hero-container">
-            <p className="about-us-hero-p text-[#5a125a]">
-              Building Community One Good Deed At A Time
-            </p>
-          </div>
-        </section>
-        <section className="about-us-mission-container">
-          <div className="about-us-mission-div-1">
-            <img src={GD} alt="" className="about-us-mission-image" />
-          </div>
-          <div className="about-us-mission-div-2">
-            <h3 className="about-us-mission-h3">Our Mission</h3>
-
-            <p className="about-us-mission-p">
-              Our mission is to connect people from all walks of life,
-              backgrounds, and experiences through volunteerism. We strive to
-              build a more connected and compassionate community by facilitating
-              volunteer opportunities that bring people together to address
-              common challenges, support local causes, and make a positive
-              impact. By fostering a culture of giving, collaboration, and
-              empathy, we aim to inspire individuals to take action, create
-              meaningful connections, and build a better world for everyone.
-            </p>
-          </div>
-        </section>
-        <section>
-          <div className="about-us-history-container">
-            <div className="about-us-history-div-1">
-              <h3 className="about-us-history-h3">Our History</h3>
-
-              <p className="about-us-history-p">
-                We were searching for meaningful ways to give back to our
-                community. We looked into various volunteer opportunities, but
-                we found that most organizations were focused on specific causes
-                and required a long-term commitment, which didn't fit with our
-                busy schedules. We realized that there was a gap in our
-                community for a flexible, short-term volunteer organization that
-                could match volunteers with a variety of different people and
-                projects. So, we decided to start our own volunteer
-                organization. Together, we formed a small team and started
-                researching the needs of our community. We found that there are
-                so many people that could benefit from short-term volunteer
-                help. We created an app to spread the word and promote our
-                cause. We are now able to match volunteers with a variety of
-                people in their community. Over time, we hope our organization
-                will grow and we are able to expand our services to other
-                communities in need and make a meaningful impact on the world.
+          sx={{
+            height: "vh",
+            display: "flex",
+            maxWidth: "100%",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingBottom: 30,
+            background:
+              "linear-gradient(90deg, rgba(255,232,210,1) 0%, rgba(196,252,240,1) 100%)",
+            color: "black",
+          }}
+        >
+          <section>
+            <div className="about-us-hero-container">
+              <p className="about-us-hero-p text-[#5a125a]">
+                Building Community One Good Deed At A Time
               </p>
             </div>
-            <div className="about-us-history-div-2">
-              <img src={Maya} alt="" className="about-us-history-image" />
+          </section>
+          <section className="about-us-mission-container">
+            <div className="about-us-mission-div-1">
+              <img src={GD} alt="" className="about-us-mission-image" />
             </div>
-          </div>
-        </section>
-        <section>
-          <div className="about-us-team-container">
-            <div>
-              <h3 className="about-us-team-h3">Leadership Members</h3>
-            </div>
+            <div className="about-us-mission-div-2">
+              <h3 className="about-us-mission-h3">Our Mission</h3>
 
-            <div className="about-us-team-cards">
-              <Card className="about-us-card">
-                <Card.Img
-                  className="about-us-team-images"
-                  variant="top"
-                  src={Lala}
-                />
-                <Card.Body className="flex justify-center items-center">
-                  <Card.Text className="about-us-card-text">
-                    Lesley-Ann Christl
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card className="about-us-card">
-                <Card.Img
-                  className="about-us-team-images"
-                  variant="top"
-                  src={Kirk}
-                />
-                <Card.Body className="flex justify-center items-center">
-                  <Card.Text className="about-us-card-text">
-                    Kirk McDowell
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card className="about-us-card">
-                <Card.Img
-                  className="about-us-team-images"
-                  variant="top"
-                  src={Rami}
-                />
-                <Card.Body className="flex justify-center items-center">
-                  <Card.Text className="about-us-card-text">
-                    Rami Basheer
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card className="about-us-card">
-                <Card.Img
-                  className="about-us-team-images"
-                  variant="top"
-                  src={Boi}
-                />
-                <Card.Body className="flex justify-center items-center">
-                  <Card.Text className="about-us-card-text">
-                    Boi Schrader
-                  </Card.Text>
-                </Card.Body>
-              </Card>
+              <p className="about-us-mission-p">
+                Our mission is to connect people from all walks of life,
+                backgrounds, and experiences through volunteerism. We strive to
+                build a more connected and compassionate community by
+                facilitating volunteer opportunities that bring people together
+                to address common challenges, support local causes, and make a
+                positive impact. By fostering a culture of giving,
+                collaboration, and empathy, we aim to inspire individuals to
+                take action, create meaningful connections, and build a better
+                world for everyone.
+              </p>
             </div>
-          </div>
-        </section>
+          </section>
+          <section className="">
+            <div className="about-us-history-container">
+              <div className="about-us-history-div-1">
+                <h3 className="about-us-history-h3">Our History</h3>
+
+                <p className="about-us-history-p">
+                  We were searching for meaningful ways to give back to our
+                  community. We looked into various volunteer opportunities, but
+                  we found that most organizations were focused on specific
+                  causes and required a long-term commitment, which didn't fit
+                  with our busy schedules. We realized that there was a gap in
+                  our community for a flexible, short-term volunteer
+                  organization that could match volunteers with a variety of
+                  different people and projects. So, we decided to start our own
+                  volunteer organization. Together, we formed a small team and
+                  started researching the needs of our community. We found that
+                  there are so many people that could benefit from short-term
+                  volunteer help. We created an app to spread the word and
+                  promote our cause. We are now able to match volunteers with a
+                  variety of people in their community. Over time, we hope our
+                  organization will grow and we are able to expand our services
+                  to other communities in need and make a meaningful impact on
+                  the world.
+                </p>
+              </div>
+              <div className="about-us-history-div-2">
+                <img src={Maya} alt="" className="about-us-history-image" />
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="about-us-team-container">
+              <div>
+                <h3 className="about-us-team-h3">Leadership Members</h3>
+              </div>
+
+              <div className="about-us-team-cards">
+                <Card className="about-us-card">
+                  <Card.Img
+                    className="about-us-team-images"
+                    variant="top"
+                    src={Lala}
+                  />
+                  <Card.Body className="flex justify-center items-center">
+                    <Card.Text className="about-us-card-text">
+                      Lesley-Ann Christl
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="about-us-card">
+                  <Card.Img
+                    className="about-us-team-images"
+                    variant="top"
+                    src={Kirk}
+                  />
+                  <Card.Body className="flex justify-center items-center">
+                    <Card.Text className="about-us-card-text">
+                      Kirk McDowell
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="about-us-card">
+                  <Card.Img
+                    className="about-us-team-images"
+                    variant="top"
+                    src={Rami}
+                  />
+                  <Card.Body className="flex justify-center items-center">
+                    <Card.Text className="about-us-card-text">
+                      Rami Basheer
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="about-us-card">
+                  <Card.Img
+                    className="about-us-team-images"
+                    variant="top"
+                    src={Boi}
+                  />
+                  <Card.Body className="flex justify-center items-center">
+                    <Card.Text className="about-us-card-text">
+                      Boi Schrader
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+          </section>
         </Box>
         <Footer2 />
       </div>
